15e1d60a5e98286be8dcccab6c43f8b2b02acc36
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-09 15:00:12 +0530

    Made Changes

--- src/Home.js
+++ src/Home.js
@@ -35,35 +35,35 @@ const Home = () => {
                     <div className='project_each'>
                         <img src={require("./images/Wallpaper_App.png")} alt='Wallpaper App' style={{width:"100%"}}/>
                         <div className='content'>
-                            <a href='https://wallpaper-app-06-03.netlify.app/' target='_blank'><h4>Wallpaper App <span class="material-symbols-outlined">link</span></h4></a>
+                            <a href='https://wallpaper-app-06-03.netlify.app/' target='_blank' rel='noopener noreferrer'><h4>Wallpaper App <span class="material-symbols-outlined">link</span></h4></a>
                             <p>HTML+CSS+JS+React+NodeJS+MongoDB+JWT</p>
                         </div>
                     </div>
                     <div className='project_each'>
                         <img src={require("./images/To_Do_List.png")} alt='To_Do_List App' style={{width:"100%"}}/>
                         <div className='content'>
-                            <a href='https://to-do-list-06-03.netlify.app' target='_blank'><h4>ToDoList App <span class="material-symbols-outlined">link</span></h4></a>
+                            <a href='https://to-do-list-06-03.netlify.app' target='_blank' rel='noopener noreferrer'><h4>ToDoList App <span class="material-symbols-outlined">link</span></h4></a>
                             <p>HTML+CSS+JS+React+NodeJS+MongoDB+JWT</p>
                         </div>
                     </div>
                     <div className='project_each'>
                         <img src={require("./images/WorkoutTracker.png")} alt='WOrkout Tracker App' style={{width:"100%"}}/>
                         <div className='content'>
-                            <a href='https://workout-tracker-06-03.netlify.app/' target='_blank'><h4>Workout Tracker App <span class="material-symbols-outlined">link</span></h4></a>
+                            <a href='https://workout-tracker-06-03.netlify.app/' target='_blank' rel='noopener noreferrer'><h4>Workout Tracker App <span class="material-symbols-outlined">link</span></h4></a>
                             <p>HTML+CSS+JS+React+NodeJS+MongoDB+JWT</p>
                         </div>
                     </div>
                     <div className='project_each'>
                         <img src={require("./images/Notes_App.png")} alt='Notes App' style={{width:"100%"}}/>
                         <div className='content'>
-                            <a href='https://notes-app-06-03.netlify.app/' target='_blank'><h4>Notes App <span class="material-symbols-outlined link_icon">link</span></h4></a>
+                            <a href='https://notes-app-06-03.netlify.app/' target='_blank' rel='noopener noreferrer'><h4>Notes App <span class="material-symbols-outlined link_icon">link</span></h4></a>
                             <p>HTML+CSS+JS+React+NodeJS+MongoDB+JWT</p>
                         </div>
                     </div>
                     <div className='project_each'>
                         <img src={require("./images/Book_store.png")} alt='Book Store App' style={{width:"100%"}}/>
                         <div className='content'>
-                            <a href='https://book-store-06-03.netlify.app/' target='_blank'><h4>Book Store App <span class="material-symbols-outlined link_icon">link</span></h4></a>
+                            <a href='https://book-store-06-03.netlify.app/' target='_blank' rel='noopener noreferrer'><h4>Book Store App <span class="material-symbols-outlined link_icon">link</span></h4></a>
                             <p>HTML+CSS+JS+React+NodeJS+MongoDB+JWT</p>
                         </div>
                     </div>
@@ -73,14 +73,14 @@ const Home = () => {
             
             <div className='links'>
                 <h3>Certifications.</h3>
-                <a href='https://www.udemy.com/certificate/UC-d0914537-adbb-40c6-81f5-f1f03898a7ed/'>Full Stack Web Development <span class="material-symbols-outlined">link</span></a>
+                <a href='https://www.udemy.com/certificate/UC-d0914537-adbb-40c6-81f5-f1f03898a7ed/' target='_blank' rel='noopener noreferrer'>Full Stack Web Development <span class="material-symbols-outlined">link</span></a>
             </div>
 
             <div className='links'>
                 <h3>Links.</h3>
                 <div className='links_div'>
-                    <a href='mailto:jeman060397@gmail.com'><img src={require('./images/gmail.png')} alt='gmail' width="24" height="24"/></a>
-                    <a href='https://github.com/Jeman-Kumar-97-06'><img src={require('./images/github.png')} alt='github' width="24" height="24"/></a>
+                    <a href='mailto:jeman060397@gmail.com' target='_blank' rel='noopener noreferrer'><img src={require('./images/gmail.png')} alt='gmail' width="24" height="24"/></a>
+                    <a href='https://github.com/Jeman-Kumar-97-06' target='_blank' rel='noopener noreferrer'><img src={require('./images/github.png')} alt='github' width="24" height="24"/></a>
                 </div>
             </div>
         </div>
