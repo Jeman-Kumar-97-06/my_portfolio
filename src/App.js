ea43e5d9d3cd10a17faa8769852ee8d22719f6b6
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-01-02 22:46:50 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>&copy;Jeman_06_03_97</p>
+        </div>
+    )
+}
+
+export default Footer;
